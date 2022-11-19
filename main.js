f101b931da8297ed177ee53abd1a33c247f6c0e1
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-18 22:27:16 -0300

    starting js logic

---
+++ main.js
@@ -0,0 +1,16 @@
+const btn1 = document.getElementById("1");
+const btn2 = document.getElementById("2");
+const btn3 = document.getElementById("3");
+const btn4 = document.getElementById("4");
+const btn5 = document.getElementById("5");
+
+let btns = [btn1, btn2, btn3, btn4, btn5];
+
+for (let i = 0; i < btns.length; i++) {
+    const forBtns = btns[i];
+    function changeAppearance() {
+        
+    }
+    forBtns.addEventListener("click", )
+}
+
