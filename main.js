bf8a8344b02511edb8cbce33612b507b56372a1e
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-20 23:47:57 -0300

    code improvement

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const btn3 = document.getElementById("3");
 const btn4 = document.getElementById("4");
 const btn5 = document.getElementById("5");
 
+btn1.addEventListener("click", () => changeAppearance(btn1));
+btn2.addEventListener("click", () => changeAppearance(btn2));
+btn3.addEventListener("click", () => changeAppearance(btn3));
+btn4.addEventListener("click", () => changeAppearance(btn4));
+btn5.addEventListener("click", () => changeAppearance(btn5));
+
+/*
 btn1.addEventListener("click", function () {
   changeAppearance(btn1);
 });
@@ -23,6 +30,7 @@ btn4.addEventListener("click", function () {
 btn5.addEventListener("click", function () {
   changeAppearance(btn5);
 });
+*/
 
 function changeAppearance(btn) {
   if (btn.className == "rating__btn") {
